ef3775b4ffbb5734848085847b3a5ccf21694547
Vishnupriya <98033624+Vishnupriya74@users.noreply.github.com>
2022-01-20 16:50:02 +0530

    Add files via upload

---
+++ Shiftoperator.java
@@ -0,0 +1,15 @@
+
+public class Shiftoperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte x, y;  
+		x=10;  
+		y=-10;  
+		System.out.println("Bitwise Left Shift: x<<2 = "+(x<<2));  
+		System.out.println("Bitwise Right Shift: x>>2 = "+(x>>2));  
+		System.out.println("Bitwise Zero Fill Right Shift: x>>>2 = "+(x>>>2));  
+		System.out.println("Bitwise Zero Fill Right Shift: y>>>2 = "+(y>>>2));  
+	}
+
+}
